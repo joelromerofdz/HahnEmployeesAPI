495211a198ca2d503fc9671c9978d14daa5c4d7f
joel-r16 <joel-r16@hotmail.com>
2023-04-25 12:18:11 -0400

    Create entities in domain layer

---
+++ HahnEmployeesAPI.Domain/Entities/Role.cs
@@ -0,0 +1,16 @@
+﻿using HahnEmployeesAPI.Domain.Base;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HahnEmployeesAPI.Domain.Entities
+{
+    public class Role : BaseEntity
+    {
+        public string Title { get; set; }
+        public string Description { get; set; }
+        public List<Employee> Employees { get; set; }
+    }
+}
