1d18184dd690b30d240dc649430b326eb529ec82
joel-r16 <joel-r16@hotmail.com>
2023-04-25 11:51:02 -0400

    Create base entity

---
+++ HahnEmployeesAPI.Domain/Base/BaseEntity.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HahnEmployeesAPI.Domain.Base
+{
+    public abstract class BaseEntity
+    {
+        private DateTime _created;
+
+        public BaseEntity()
+        {
+            this.Created = DateTime.Now;
+        }
+
+        public int Id { get; set; }
+        public DateTime Created 
+        { 
+            get => this._created;
+            set => this._created = value;
+        }
+        public DateTime? Updated { get; set; }
+    }
+}
