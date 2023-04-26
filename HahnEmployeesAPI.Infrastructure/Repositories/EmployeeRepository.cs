2f711f18ebf9c2d4ed810393dc9630cfa366b6f9
joel-r16 <joel-r16@hotmail.com>
2023-04-26 17:24:39 -0400

    Create repository base and repositories

---
+++ HahnEmployeesAPI.Infrastructure/Repositories/EmployeeRepository.cs
@@ -0,0 +1,19 @@
+﻿using HahnEmployeesAPI.Domain.Entities;
+using HahnEmployeesAPI.Infrastructure.Base;
+using HahnEmployeesAPI.Infrastructure.Data;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HahnEmployeesAPI.Infrastructure.Repositories
+{
+    public class EmployeeRepository : RepositoryBase<Employee>
+    {
+        public EmployeeRepository(AppDbContext dbContext) : base(dbContext)
+        {
+
+        }
+    }
+}
